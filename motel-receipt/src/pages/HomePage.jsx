73cfc1b9bc04e781dfbac868a8bc2d02a5f94b59
Tin0709 <2002trungtin@gmail.com>
2026-05-04 20:56:43 +0700

    disable browser translation for now

--- motel-receipt/src/pages/HomePage.jsx
+++ motel-receipt/src/pages/HomePage.jsx
@@ -585,7 +585,7 @@ export default function HomePage() {
       clearDraftToastOpenRaf();
     };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    /// eslint-disable-next-line react-hooks/exhaustive-deps
   }, [draftToast]);
 
   const refreshData = async () => {
@@ -670,7 +670,7 @@ export default function HomePage() {
       clearDraftToastRaf();
       clearDraftToastOpenRaf();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    /// eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const activeAddRoomBlock = useMemo(() => {
